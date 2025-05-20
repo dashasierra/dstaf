c1f1114c52408c68933799242a3da4c0b0fea159
Dasha Sierra <33302393+dashasierra@users.noreply.github.com>
2025-05-20 20:39:11 +1000

    Linting fixes and license changes
    Linting fixes and license changes

--- dstaf/mouse.py
+++ dstaf/mouse.py
@@ -1,6 +1,7 @@
 import platform
-from abc import ABC, abstractmethod
+from abc import ABC
 from enum import Enum, EnumType
+
 
 class _MouseEvent(ABC):
 
@@ -15,19 +16,26 @@ class _MouseEvent(ABC):
         hold = 0.5
         release = 0.25
 
-    def get_mouse_button(self, key_value: int, ignore_errors: bool = None) -> [ClickType, Click]:
+    def get_mouse_button(
+        self, key_value: int, ignore_errors: bool = None
+    ) -> [ClickType, Click]:
         if not isinstance(ignore_errors, bool):
             ignore_errors = self._ignore_errors
         for click_type in self.ClickType:
-            if key_value in [key.value*click_type.value for key in self.Click]:
+            if key_value in [key.value * click_type.value for key in self.Click]:
                 return click_type, self.Click(key_value)
             if not ignore_errors:
-                raise ValueError(f"The mouse button value '{key_value}' is not a known mouse button type")
+                raise ValueError(
+                    f"The mouse button value '{key_value}' is not a known mouse button type"
+                )
             return self.ClickType.none, self.Click.none
 
     def __init_subclass__(cls, **kwargs):
         if not hasattr(cls, "Click"):
-            raise NotImplementedError(f"Click(Enum) not specified in {cls.__name__}")
+            raise NotImplementedError(
+                f"Click(Enum) not specified in {
+                    cls.__name__}"
+            )
         if not isinstance(cls.Click, EnumType):
             # The Click class should be an enumerator
             raise TypeError(f"{cls.__name__}.Click is not of type Enum")
@@ -35,7 +43,10 @@ class _MouseEvent(ABC):
             raise NameError(f"{cls.__name__}.Click.none must exist")
         if cls.Click.none.value == -9:
             # This means the default Click class is present, so:
-            raise NotImplementedError(f"Click(Enum) not specified in {cls.__name__}")
+            raise NotImplementedError(
+                f"Click(Enum) not specified in {
+                    cls.__name__}"
+            )
         if "ignore_errors" in kwargs.keys():
             cls._ignore_errors = kwargs["ignore_errors"]
         else:
@@ -48,6 +59,7 @@ class _WindowsMouseEvent(_MouseEvent):
         """
         Mouse Click Integer Values for Windows Terminals
         """
+
         none = 0
         left = 1
         right = 2
@@ -62,6 +74,7 @@ class _CursesMouseEvent(_MouseEvent):
         """
         Mouse Click Integer Values for Curses
         """
+
         none = 0
         left = 4
         right = 4096
